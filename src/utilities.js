3aa72f9f68109718fcc096bca528162fe13849fc
AlfredoRG <alfredoramirezg21@gmail.com>
2021-11-20 22:05:23 -0600

    first commit

---
+++ src/utilities.js
@@ -0,0 +1,20 @@
+export const drawRect = (detections, ctx) =>{
+  // Loop through each prediction
+  detections.forEach(prediction => {
+
+    // Extract boxes and classes
+    const [x, y, width, height] = prediction['bbox'];
+    const text = prediction['class'];
+
+    // Set styling
+    ctx.strokeStyle = '#E63CC3'
+    ctx.font = '18px Arial';
+
+    // Draw rectangles and text
+    ctx.beginPath();
+    ctx.fillStyle = '#E63CC3'
+    ctx.fillText(text, x, y);
+    ctx.rect(x, y, width, height);
+    ctx.stroke();
+  });
+}
